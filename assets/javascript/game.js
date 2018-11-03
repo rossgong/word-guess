f48a3eef38ca30a967453b62b2283844bb996547
Ross Gongaware <rossgong@gmail.com>
2018-11-03 09:44:16 -0400

    Created base game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,10 @@
-var dict = ["test"];
+
+//dictionary of possible words
+var dict = ["ross", "gongaware", "test", "content", "wowza"];
 
 var currentWord;
 var guessedLetters;
+var wrongGuesses;
 
 
 /*
@@ -10,30 +13,114 @@ var guessedLetters;
 function setNewWord() {
     currentWord = makeWord(getWord());
     guessedLetters = [];
+    wrongGuesses = 0;
+    
+    updateHTML();
 }
 
 /*
     Gets a random word from the dictionary.
 */
 function getWord() {
-    return dict[Math.floor(Math.random()*dict.length)];
+    return dict[Math.floor(Math.random() * dict.length)];
 }
 
 /*
     Makes a word object containing the word and an arry of blanks.
 */
-function makeWord (newWord) {
+function makeWord(newWord) {
     var newBlanks = [];
     for (var i = 0; i < newWord.length; i++) {
         newBlanks.push("_");
     }
     return {
-        word: newWord,
+        word: newWord.split(""),
         blanks: newBlanks
     };
 }
 
-setNewWord()
-console.log(currentWord);
-console.log(guessedLetters);
+/*
+    function that works the logic to guessing a letter. Will check the word and fill in the blanks. 
+    Also correctly increments the wrong guesses.
+*/
+function guessLetter(letter) {
+    if (typeof letter === "string" && letter.length === 1 /*&& isLetter(letter)*/) {
+        if (guessedLetters.indexOf(letter) === -1) {
+            guessedLetters.push(letter);
+            var isCorrect = false;
+            for (var i = 0; i < currentWord.word.length; i++) {
+                if (currentWord.word[i] === letter) {
+                    currentWord.blanks[i] = letter;
+                    isCorrect = true;
+                }
+            }
+            if (!isCorrect) {
+                wrongGuesses++;
+            }
+        }
+    }
+}
 
+/*
+    Update the HTML based on the currentWord status
+*/
+function updateHTML() {
+    var blankDiv = document.getElementById("blanks-div");
+    var guessDiv = document.getElementById("guesses-div");
+
+    var blankString = "";
+    var guessString = "";
+
+    currentWord.blanks.forEach(blank => {
+        blankString += blank + " ";
+    });
+
+    blankDiv.textContent = blankString;
+
+    guessedLetters.forEach(letter => {
+        guessString += letter + " ";
+    });
+
+    guessDiv.textContent = guessString;
+}
+
+/*
+    called when you lose
+*/
+function showLose() {
+    alert("youlose");
+}
+
+/*
+    called if you win
+*/
+function showWin() {
+    alert("youwin");
+}
+
+/*
+    Determines if the player have won and tell the player.
+*/
+function gameResolution() {
+    if (wrongGuesses > 4) {
+        showLose();
+    } else if (currentWord.blanks.indexOf("_") == -1) {
+        showWin();
+    } else {
+        return;
+    }
+    setNewWord();
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        document.onkeyup = function (event) {
+            var key = event.key;
+            guessLetter(key);
+            updateHTML();
+            gameResolution();
+        };
+
+        setNewWord();
+    }
+};
